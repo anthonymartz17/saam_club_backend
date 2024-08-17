1c4173504fe09c04574d5eb3562f3a7ccb35ddf2
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-17 12:55:12 -0400

    quick fix

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,7 @@ CREATE DATABASE saam_club_db;
 -- Create users table:
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    uid VARCHAR(255) UNIQUE NOT NULL,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
