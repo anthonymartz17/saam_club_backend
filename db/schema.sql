6c3689f8b2d27289757b12c5f2f8febfe704a96e
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-18 15:00:44 -0400

    changes have been made to schema and data has been updated in seed file

--- db/schema.sql
+++ db/schema.sql
@@ -7,37 +7,39 @@ CREATE DATABASE saam_club_db;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     uid VARCHAR(255) UNIQUE NOT NULL,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50),
+    bio TEXT,
     email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 -- Create posts table:
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
-    title VARCHAR(255) NOT NULL,
+    user_uid VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 -- Create comments table:
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
     posts_id INTEGER REFERENCES posts(id),
-    parent_id INTEGER REFERENCES comments(id),
+    user_id INTEGER REFERENCES users(id),
+    user_uid VARCHAR(255) NOT NULL,
+    parent_comment_id INTEGER REFERENCES comments(id),
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 -- Create likes table:
 CREATE TABLE likes (
-    id SERIAL PRIMARY KEY,
+    PRIMARY KEY (user_id, posts_id),
     user_id INTEGER REFERENCES users(id),
     posts_id INTEGER REFERENCES posts(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
