05ddd88be662829d78411a1a116775f419483fae
antonio <antonio.fr.martinezc@hotmail.com>
2024-08-21 19:14:23 -0400

    updated posts_id to post_id on posts query

--- queries/posts.queries.js
+++ queries/posts.queries.js
@@ -16,9 +16,9 @@ const getAllPosts = async () => {
         LEFT JOIN
             users ON posts.user_id = users.id
         LEFT JOIN
-            likes ON posts.id = likes.posts_id
+            likes ON posts.id = likes.post_id
         LEFT JOIN
-            comments ON posts.id = comments.posts_id
+            comments ON posts.id = comments.post_id
         GROUP BY
             posts.id, posts.user_uid, posts.content, posts.created_at, posts.updated_at, users.username
         ORDER BY
@@ -58,9 +58,9 @@ const getPost = async (id) => {
         LEFT JOIN 
             users ON posts.user_id = users.id
         LEFT JOIN 
-            likes ON posts.id = likes.posts_id
+            likes ON posts.id = likes.post_id
         LEFT JOIN 
-            comments ON posts.id = comments.posts_id
+            comments ON posts.id = comments.post_id
         WHERE
             posts.id = $1
         GROUP BY 
